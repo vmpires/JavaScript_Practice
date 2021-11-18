a1847ed9ce5553ac9f7f3267052da1327644683a
unknown <vctprs@gmail.com>
2021-11-18 00:16:49 -0300

    Function practice 2

--- Javascript Functions/script.js
+++ Javascript Functions/script.js
@@ -1,1 +1,8 @@
-alert("Olá!");
+function onSubmit(){
+    let peso = document.getElementById("weight").value
+    let altura = document.getElementById("height").value
+
+    let imc = peso / (altura**2);
+
+    alert(imc)
+}
